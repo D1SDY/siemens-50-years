7b0bdec82b123113f2e81608fe85de1b2999e9b8
Anton Striapan <striaant@fel.cvut.cz>
2020-10-20 10:43:12 +0200

    added react

---
+++ 50-years-React/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import Siemens50Years from "./components/Siemens-50-years";
+
+function App() {
+  return (
+   <Siemens50Years></Siemens50Years>
+  );
+}
+
+export default App;
