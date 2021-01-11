bbd4f497671f4c3f68c34413f60280d86020b9f6
Anton Striapan <striaant@fel.cvut.cz>
2021-01-11 11:09:46 +0100

    added build

---
+++ 50-years-React/build/precache-manifest.f41f185b4be59616361d71c502b3e2e1.js
@@ -0,0 +1,342 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9a42ad510ecdde97ea7cfd5e6ad2f31",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b382e282a77982c0876",
+    "url": "/static/css/main.2cd34b4a.chunk.css"
+  },
+  {
+    "revision": "39b1a47ac10231cf78c6",
+    "url": "/static/js/2.160930d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.160930d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b382e282a77982c0876",
+    "url": "/static/js/main.a6c191f7.chunk.js"
+  },
+  {
+    "revision": "eead869515ade6d65c13",
+    "url": "/static/js/runtime-main.fe7d8410.js"
+  },
+  {
+    "revision": "dfa16d1d7319f4111006e4b2804f9269",
+    "url": "/static/media/1980_Image.dfa16d1d.jpg"
+  },
+  {
+    "revision": "424e05d81a36c514e048c0798b98310d",
+    "url": "/static/media/4thIndustrialRevolution_Placeholder_Image_13.424e05d8.jpg"
+  },
+  {
+    "revision": "8beb432bb4db3a6ebffeae63910a5665",
+    "url": "/static/media/5G_Placeholder_Image_14.8beb432b.jpg"
+  },
+  {
+    "revision": "109a36884e117d08aa42418dc3c08603",
+    "url": "/static/media/AIAssitan_Image_13.109a3688.jpg"
+  },
+  {
+    "revision": "71cd1872d073ba21415cb6c8464a8dee",
+    "url": "/static/media/Amazon_Image_10.71cd1872.jpg"
+  },
+  {
+    "revision": "1dc678fd02ea0e0e843c15bc434561ee",
+    "url": "/static/media/Chatbots_Image_13.1dc678fd.jpg"
+  },
+  {
+    "revision": "204c5aaedf3f5fa94cc459d310044b68",
+    "url": "/static/media/FirstCellphone_Image_6.204c5aae.jpg"
+  },
+  {
+    "revision": "fb1785c57911893014fa29143ce41f4c",
+    "url": "/static/media/FirstDigitalMessage_Image_4.fb1785c5.jpg"
+  },
+  {
+    "revision": "ddd533bb3f00cfb18ae3142dc1b17273",
+    "url": "/static/media/FirstMouse_Image_3.ddd533bb.jpg"
+  },
+  {
+    "revision": "3a6e76cf4bdf5d0a0dd6e1b1a339dee1",
+    "url": "/static/media/Floppydisk_Image_5.3a6e76cf.jpg"
+  },
+  {
+    "revision": "475f5c70605e0c60e2ae302cc4ebf0ee",
+    "url": "/static/media/Interfaces_Image_15.475f5c70.jpg"
+  },
+  {
+    "revision": "4dd540fac413033980dc9673fb004145",
+    "url": "/static/media/Mocrochip_Image_2.4dd540fa.jpg"
+  },
+  {
+    "revision": "5cf724c8ed8d7aa06455ae6d0340b231",
+    "url": "/static/media/SiemensSans-Black.5cf724c8.ttf"
+  },
+  {
+    "revision": "81fc7cbbf22cefea428a2d4fe7370b1b",
+    "url": "/static/media/SiemensSans-Black.81fc7cbb.woff"
+  },
+  {
+    "revision": "ab1e9e1da06a3c4fcaa11199dff3783e",
+    "url": "/static/media/SiemensSans-Black.ab1e9e1d.eot"
+  },
+  {
+    "revision": "84c81f524f71cd0619d4cc7f6516cabb",
+    "url": "/static/media/SiemensSans-BlackItalic.84c81f52.woff"
+  },
+  {
+    "revision": "8aad11ce0078df0349b1019059e1e3f4",
+    "url": "/static/media/SiemensSans-BlackItalic.8aad11ce.eot"
+  },
+  {
+    "revision": "f7996b4d30e9a31c6f87b537b893b895",
+    "url": "/static/media/SiemensSans-BlackItalic.f7996b4d.ttf"
+  },
+  {
+    "revision": "377b8df2c28b6e8c1dca03e0659ebe2b",
+    "url": "/static/media/SiemensSans-Bold.377b8df2.ttf"
+  },
+  {
+    "revision": "924a755d4cbcf5eb2b7478295db11a32",
+    "url": "/static/media/SiemensSans-Bold.924a755d.woff"
+  },
+  {
+    "revision": "f6058cbbd0483dc67e1262edbdc3c960",
+    "url": "/static/media/SiemensSans-Bold.f6058cbb.eot"
+  },
+  {
+    "revision": "12c7df718e5e944f30218ea7c6f76626",
+    "url": "/static/media/SiemensSans-BoldItalic.12c7df71.ttf"
+  },
+  {
+    "revision": "a1554405d4284dfa03bb6ef9e1a809b8",
+    "url": "/static/media/SiemensSans-BoldItalic.a1554405.eot"
+  },
+  {
+    "revision": "c7cc9441063c1b47d512676cbd8c2876",
+    "url": "/static/media/SiemensSans-BoldItalic.c7cc9441.woff"
+  },
+  {
+    "revision": "2d33db40a966470d2ffe44e879da6879",
+    "url": "/static/media/SiemensSans-Italic.2d33db40.eot"
+  },
+  {
+    "revision": "65b293f7bb10b569f50b3d3ee7b91d5c",
+    "url": "/static/media/SiemensSans-Italic.65b293f7.ttf"
+  },
+  {
+    "revision": "e48b349fce5d489c2e99925cfc8203e7",
+    "url": "/static/media/SiemensSans-Italic.e48b349f.woff"
+  },
+  {
+    "revision": "4ed26c83ea302f2f4e4a03a6ea79969c",
+    "url": "/static/media/SiemensSans-Roman.4ed26c83.woff"
+  },
+  {
+    "revision": "53db8c9105bdb3c2bd67a928800eec99",
+    "url": "/static/media/SiemensSans-Roman.53db8c91.eot"
+  },
+  {
+    "revision": "cb145d06ae4f6671921a88a10555a82b",
+    "url": "/static/media/SiemensSans-Roman.cb145d06.ttf"
+  },
+  {
+    "revision": "1107f1b102c3bbcf147dafa9486e2e91",
+    "url": "/static/media/SiemensSerif-Bold.1107f1b1.woff"
+  },
+  {
+    "revision": "42076de8e8d90d16203491e8794c92a3",
+    "url": "/static/media/SiemensSerif-Bold.42076de8.ttf"
+  },
+  {
+    "revision": "61e9dd76561539b9084f5ccdc8faa80b",
+    "url": "/static/media/SiemensSerif-Bold.61e9dd76.eot"
+  },
+  {
+    "revision": "07471b34109c7128dbdaf79af23eb753",
+    "url": "/static/media/SiemensSerif-BoldItalic.07471b34.ttf"
+  },
+  {
+    "revision": "50762c3e9bfe6a92fd07afccd4d7021e",
+    "url": "/static/media/SiemensSerif-BoldItalic.50762c3e.woff"
+  },
+  {
+    "revision": "dd143bbee0075d2df3c339fce6c4f9d3",
+    "url": "/static/media/SiemensSerif-BoldItalic.dd143bbe.eot"
+  },
+  {
+    "revision": "34a96f6137717e275a03f22fc10e71ab",
+    "url": "/static/media/SiemensSerif-Italic.34a96f61.eot"
+  },
+  {
+    "revision": "73df46546f8c061c6a0c94f14f63b2b7",
+    "url": "/static/media/SiemensSerif-Italic.73df4654.woff"
+  },
+  {
+    "revision": "8a8f3bc2a2bda80145ec52ac014308ce",
+    "url": "/static/media/SiemensSerif-Italic.8a8f3bc2.ttf"
+  },
+  {
+    "revision": "500224604e0936a3a2926ec41d37ae43",
+    "url": "/static/media/SiemensSerif-Roman.50022460.ttf"
+  },
+  {
+    "revision": "97f40885b17758cd34187ce3591e5cd1",
+    "url": "/static/media/SiemensSerif-Roman.97f40885.woff"
+  },
+  {
+    "revision": "ef6402a7f68b3e3f27328714cfb360a0",
+    "url": "/static/media/SiemensSerif-Roman.ef6402a7.eot"
+  },
+  {
+    "revision": "6e81da0adf909af6cde4bc24f300dd56",
+    "url": "/static/media/SiemensSerif-Semibold.6e81da0a.woff"
+  },
+  {
+    "revision": "7745983f92f2e344242184bcf5876df2",
+    "url": "/static/media/SiemensSerif-Semibold.7745983f.eot"
+  },
+  {
+    "revision": "d8f4522298875f55eae325a2e448e8eb",
+    "url": "/static/media/SiemensSerif-Semibold.d8f45222.ttf"
+  },
+  {
+    "revision": "84a92bf2c3b1a80f169d090242028725",
+    "url": "/static/media/SiemensSerif-SemiboldItalic.84a92bf2.woff"
+  },
+  {
+    "revision": "d0b8247ed215943876c4a5e03aae47f3",
+    "url": "/static/media/SiemensSerif-SemiboldItalic.d0b8247e.eot"
+  },
+  {
+    "revision": "f82979f9f6295a904cecbf9d93a3e82c",
+    "url": "/static/media/SiemensSerif-SemiboldItalic.f82979f9.ttf"
+  },
+  {
+    "revision": "662ddca21bca318490749a399db1018a",
+    "url": "/static/media/SiemensSlab-Black.662ddca2.woff"
+  },
+  {
+    "revision": "e064392937401ea19d09ebd45aab6f9a",
+    "url": "/static/media/SiemensSlab-Black.e0643929.eot"
+  },
+  {
+    "revision": "e6e1b52a760fcb479570491e49055047",
+    "url": "/static/media/SiemensSlab-Black.e6e1b52a.ttf"
+  },
+  {
+    "revision": "6489371af2168d6adecf5f7740b5ab25",
+    "url": "/static/media/SiemensSlab-BlackItalic.6489371a.eot"
+  },
+  {
+    "revision": "a19a4fea43dcccbc32905836a12affb8",
+    "url": "/static/media/SiemensSlab-BlackItalic.a19a4fea.woff"
+  },
+  {
+    "revision": "be027567ef01332b6f28a36866d54d12",
+    "url": "/static/media/SiemensSlab-BlackItalic.be027567.ttf"
+  },
+  {
+    "revision": "21453eac9ed1fd869ea42ca65b7ae884",
+    "url": "/static/media/SiemensSlab-Bold.21453eac.woff"
+  },
+  {
+    "revision": "c24f2c27a44ff26f68fbf4bd06246ac8",
+    "url": "/static/media/SiemensSlab-Bold.c24f2c27.eot"
+  },
+  {
+    "revision": "e9cc5c6a9c0ea078d4f2082d13cf7a92",
+    "url": "/static/media/SiemensSlab-Bold.e9cc5c6a.ttf"
+  },
+  {
+    "revision": "01044740731fa13e23db4ed8de1ba1fb",
+    "url": "/static/media/SiemensSlab-BoldItalic.01044740.eot"
+  },
+  {
+    "revision": "4d21db92f9daad0e6343825a30b62e0f",
+    "url": "/static/media/SiemensSlab-BoldItalic.4d21db92.ttf"
+  },
+  {
+    "revision": "7e31f07a0b3b708311e4502c64787f1c",
+    "url": "/static/media/SiemensSlab-BoldItalic.7e31f07a.woff"
+  },
+  {
+    "revision": "6671b1d1117c3278a767a61501ad0c74",
+    "url": "/static/media/SiemensSlab-Italic.6671b1d1.ttf"
+  },
+  {
+    "revision": "88fdd160e16f266c4209450ecb992a73",
+    "url": "/static/media/SiemensSlab-Italic.88fdd160.woff"
+  },
+  {
+    "revision": "f0fd15e0bf7bbb0fce158e8152fe89e3",
+    "url": "/static/media/SiemensSlab-Italic.f0fd15e0.eot"
+  },
+  {
+    "revision": "1f37e5a0dc22a59d295f97da760eefaf",
+    "url": "/static/media/SiemensSlab-Roman.1f37e5a0.ttf"
+  },
+  {
+    "revision": "625f0388b33cbf40ceea2cd6e338f2db",
+    "url": "/static/media/SiemensSlab-Roman.625f0388.eot"
+  },
+  {
+    "revision": "ebb8b265afb1b35e81892a77f28cff05",
+    "url": "/static/media/SiemensSlab-Roman.ebb8b265.woff"
+  },
+  {
+    "revision": "8a7c9d3d0c3bd60ce5e0ccb2a6cc8df4",
+    "url": "/static/media/SiliconElement_Image_1.8a7c9d3d.jpg"
+  },
+  {
+    "revision": "67a20e57692f6e278f4d368e5bbf3845",
+    "url": "/static/media/SmallHardware_Placeholder_Image_16.67a20e57.jpg"
+  },
+  {
+    "revision": "bd5bd5844149f5e6b69ee07ce95563cf",
+    "url": "/static/media/SocialMedia_Placeholder_Image_12.bd5bd584.jpg"
+  },
+  {
+    "revision": "60f6d05f415261cf2306e5e16a5ac039",
+    "url": "/static/media/Web2_Placeholder_Image_11.60f6d05f.jpg"
+  },
+  {
+    "revision": "d30817b296b5bd47220ad8205639a738",
+    "url": "/static/media/background-lp-16.9.d30817b2.jpg"
+  },
+  {
+    "revision": "993bd97419f5a437dba82b569a660b5a",
+    "url": "/static/media/background-lp-medium.993bd974.png"
+  },
+  {
+    "revision": "667ea417a312ac4a711eff4d7a93b034",
+    "url": "/static/media/background-lp-small.667ea417.png"
+  },
+  {
+    "revision": "b2227666254bbcda5c07b4f726eb4113",
+    "url": "/static/media/bkg-svg.b2227666.jpg"
+  },
+  {
+    "revision": "300f2795b9fe315aaf2ee8752a6207da",
+    "url": "/static/media/html_Image_9.300f2795.jpg"
+  },
+  {
+    "revision": "72a43bd8d5f4f69e64596bc28a70dd9c",
+    "url": "/static/media/simpl-icons-newton.72a43bd8.eot"
+  },
+  {
+    "revision": "a028d71db11d3b6e71591131a9d2360d",
+    "url": "/static/media/simpl-icons-newton.a028d71d.woff"
+  },
+  {
+    "revision": "da1af320f6a78a71ac161c21070b92d2",
+    "url": "/static/media/simpl-icons-newton.da1af320.ttf"
+  },
+  {
+    "revision": "87be8a517088d1d54f653a8bed7af34e",
+    "url": "/static/media/www_Image_8.87be8a51.jpg"
+  }
+]);
